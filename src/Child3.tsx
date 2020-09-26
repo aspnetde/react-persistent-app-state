7e54f43d735df523e992dc6a961f5a50d47c16a7
Thomas Bandt <bandt@69grad.de>
2020-09-26 16:09:39 +0200

    Update Child 3
    Now it gets clear that it isn't re-rendered when the state changes.

--- src/Child3.tsx
+++ src/Child3.tsx
@@ -1,14 +1,12 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext } from "react";
 import AppStateContext from "./AppContext";
 
 export default function () {
   const dispatch = useContext(AppStateContext.dispatch);
 
-  const renderTime = useMemo(() => new Date().getUTCMilliseconds(), []);
-
   return (
     <div>
-      Child 3. Rendered: {renderTime}. &nbsp;{" "}
+      Child 3. Rendered: {new Date().getUTCMilliseconds()}. &nbsp;{" "}
       <button onClick={() => dispatch({ type: "ResetComponentMsg" })}>
         Reset
       </button>
