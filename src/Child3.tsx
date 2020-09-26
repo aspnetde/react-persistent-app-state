16da66db271a4707c0fffa6a65e7cdae1f67f734
Thomas Bandt <bandt@69grad.de>
2020-09-26 16:10:31 +0200

    Revert "Update Child 3"
    This reverts commit 7e54f43d735df523e992dc6a961f5a50d47c16a7.

--- src/Child3.tsx
+++ src/Child3.tsx
@@ -1,12 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import AppStateContext from "./AppContext";
 
 export default function () {
   const dispatch = useContext(AppStateContext.dispatch);
 
+  const renderTime = useMemo(() => new Date().getUTCMilliseconds(), []);
+
   return (
     <div>
-      Child 3. Rendered: {new Date().getUTCMilliseconds()}. &nbsp;{" "}
+      Child 3. Rendered: {renderTime}. &nbsp;{" "}
       <button onClick={() => dispatch({ type: "ResetComponentMsg" })}>
         Reset
       </button>
